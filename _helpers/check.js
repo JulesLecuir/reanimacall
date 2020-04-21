8af4fcb791e7900fd64eb7295ff4ecc4b3edf4c8
Jules Lecuir <jules.lecuir@outlook.fr>
2020-04-22 01:38:16 +0200

    Improvement of logging in check functions. Removal of unused check functions for now. Will implement them when I need them.

--- _helpers/check.js
+++ _helpers/check.js
@@ -1,33 +1,12 @@
 const LOG = require('./LOG');
 module.exports = {
     str: (...args) => {
-        for (let e of args) {
-            if (typeof e !== 'string') {
-                const err = new TypeError();
+        for (let i = 0; i < args.length; i++) {
+            if (typeof args[i] !== 'string') {
+                const err = new TypeError(`Variable n°${i + 1} is not of type String`);
                 LOG.error(err);
-                throw TypeError();
+                throw err;
             }
         }
-    },
-    num: (...args) => {
-        for (let e of args) {
-            if (typeof e !== 'number') {
-                const err = new TypeError();
-                LOG.error(err);
-                throw TypeError();
-            }
-        }
-    },
-    bool: (...args) => {
-        for (let e of args) {
-            if (typeof e !== 'boolean') {
-                const err = new TypeError();
-                LOG.error(err);
-                throw TypeError();
-            }
-        }
-    },
+    }
 };
-
-module.exports.str("a", "accc", "aaecceec");
-module.exports.str("afe", 122, "éed");
