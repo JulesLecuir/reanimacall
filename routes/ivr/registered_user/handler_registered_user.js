90b91af112d16f372cce60730ca49ecf18f0c037
Jules Lecuir <jules.lecuir@outlook.fr>
2020-04-21 23:21:16 +0200

    Perfected spoken text for registered users.

--- routes/ivr/registered_user/handler_registered_user.js
+++ routes/ivr/registered_user/handler_registered_user.js
@@ -22,7 +22,7 @@ function welcome(fromNumberFormatted) {
         .say(voice.loop,
             'Bonjour. ' +
             'Vous nous appelez depuis le numéro ' + fromNumberFormatted + '.' +
-            'Entrez votre code PIN, puis tapez dièse.'
+            'Entrez votre code PINE, puis tapez dièse.'
         );
 
     return twiml.toString();
@@ -32,19 +32,18 @@ async function authPin(fromNumber, digits) {
 
     const twiml = new VoiceResponse();
 
-    await Promise.all([
-        // Authenticate the user and set it as active.
-        UserService
-            .authenticate({phone: fromNumber, pin: digits})
-            .then(() => {
-                    console.log("Authentification réussie");
-                    twiml.say(voice.normal,
-                        "Merci. Vous pouvez maintenant formuler oralement votre demande. Nous ferons ensuite passer votre message à vos contacts. Sachez également" +
-                        " que pour l'instant, vos messages ne sont pas chiffrés et peuvent faire l'objet d'attaques. Nous " +
-                        "travaillons actuellement sur un système de chiffrement des enregistrements pour une prochaine " +
-                        "version de notre service. Jules Lecuir, développeur de l'application, se dédouanne de tout préjudice," +
-                        "bien qu'il travaille du mieux qu'il peut pour vous apporter son aide! Vous pouvez maintenant parler.");
-                    twiml.record({
+    await UserService
+        .authenticate({phone: fromNumber, pin: digits, callSid})
+        .then(() => {
+                console.log("Authentification réussie");
+                // TODO invoke a function if the user presses a key that memorizes that the user doesn't want to hear the messages before leaving a message
+                twiml.say(voice.normal,
+                    "Merci. Vous pouvez maintenant formuler oralement votre demande. Nous ferons ensuite passer votre message à vos contacts. Sachez également" +
+                    " que pour l'instant, vos messages ne sont pas chiffrés et peuvent faire l'objet d'attaques. Nous " +
+                    "travaillons actuellement sur un système de chiffrement des enregistrements pour une prochaine " +
+                    "version de notre service. Jules Lecuir, développeur de l'application, se dédouanne de tout préjudice," +
+                    "bien qu'il travaille du mieux qu'il peut pour vous apporter son aide! Vous pouvez maintenant parler.");
+                twiml.record({
                             action: "/ivr/reg/thankAfterMessage",
                             recordingStatusCallback: "/users/processMessage",
                             finishOnKey: '#',
@@ -65,6 +64,8 @@ async function authPin(fromNumber, digits) {
 }
 
 function thankAfterMessage() {
+
+    // TODO replay the message that the person just recorded. If the person hans up, validates, if presses any key, can record it again.
     return new VoiceResponse()
         .say(
             voice.normal,
