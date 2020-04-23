e48d35d4eaa5f2bd832d6d81fe17fb32d20f3ef8
Jules Lecuir <jules.lecuir@outlook.fr>
2020-04-23 23:44:16 +0200

    Improved indexing of database (now has indexes based on phone and callSid)

--- db.js
+++ db.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 const LOG = require('./_helpers/LOG');
 
 module.exports = {
+
     connect: async (db_name) => {
         await mongoose
             .connect(process.env.DB_HOST + db_name, {
                 useUnifiedTopology: true,
                 useCreateIndex: true,
-                useNewUrlParser: true
+                useNewUrlParser: true,
+                autoIndex: false
             })
             .then(() => LOG.success('Connected to database ' + db_name))
             .catch((err) => LOG.error(err));
         mongoose.Promise = global.Promise;
         return mongoose.connection;
-    },
-    getConnection: () => mongoose.connection
+    }
 };
--- users/user_model.js
+++ users/user_model.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     phone: {type: String, unique: true, required: true},
     hash: {type: String, required: true},
-    firstName: {type: String},
     createdDate: {type: Date, default: Date.now()},
     contacts: {type: Array, default: []},
-    status: {type: String, enum: ['inactive', 'asking', 'waiting', 'calledBack'], default: 'inactive'},
-    callSid: {type: String, required: () => this.status !== 'inactive'},
+    status: {type: String, required: true, enum: ['inactive', 'asking', 'waiting', 'calledBack'], default: 'inactive'},
+    callSid: {type: String, unique: true, required: () => this.status !== 'inactive'}
 });
 
 userSchema.set('toJSON', {virtuals: true});
